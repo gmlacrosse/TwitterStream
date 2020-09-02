1eff50b1f70e70d74eb13b558d9d93a196bd829a
mlacrosse3 <mlacrosse3@gmail.com>
2020-09-01 20:46:44 -0500

    Updates

--- TwitterStream/Counters.cs
+++ TwitterStream/Counters.cs
@@ -13,8 +13,8 @@ namespace TwitterStream
     {
         private DateTime StartDateTime = DateTime.UtcNow;
         public int Count { get; set; }
-        public int HashTagCount { get; set; }
-        public int WithUrlsCount { get; set; }
+        public int WithHashTags { get; set; }
+        public int WithUrls { get; set; }
         public int WithMedia { get; set; }
         public int WithEmojis { get; set; }
 
@@ -29,9 +29,9 @@ namespace TwitterStream
             double minElasped = (DateTime.UtcNow - StartDateTime).TotalMinutes;
             double hourElasped = minElasped > 60 ? 0 : minElasped / 60;
 
-            int sec = 0; 
+            int sec = 0;
             int min = 0;
-            int hour= 0;
+            int hour = 0;
             string minStr = "--";
             string hourStr = "--";
             if (Count > 0)
@@ -43,7 +43,30 @@ namespace TwitterStream
                 minStr = min == 0 ? "--" : min.ToString();
                 hourStr = hour == 0 ? "--" : hour.ToString();
             }
-            var statsString = $"TotalCount : {Count}  Hour : {hourStr} | Min : {minStr} | Sec : {sec}";
+
+            var tweets = string.Format("{0, 10}", "Tweets");
+            var perhour = string.Format("{0, 7}", "Hour");
+            var permin = string.Format("{0, 7}", "Min");
+            var persec = string.Format("{0, 7}", "Sec");
+            var emojis = string.Format("{0, 7}", "Emojis");
+            var hashtags = string.Format("{0, 8}", "Hashtags");
+            var urls = string.Format("{0, 7}", "URLs");
+            var photos = string.Format("{0, 7}", "Photos");
+            var percent = string.Format("{0, 7}", @"%");
+            var emjper = WithEmojis * 100.0 / Count;
+            var emjperstr = string.Format("{0, 7}", $"{emjper:###.##}");
+            var hashper = WithHashTags * 100.0 / Count;
+            var hashperstr = string.Format("{0, 7}", $"{hashper:###.##}");
+            var urlper = WithUrls * 100.0 / Count;
+            var urlperstr = string.Format("{0, 7}", $"{urlper:###.##}");
+            var photoper = WithMedia * 100.0 / Count;
+            var photoperstr = string.Format("{0, 7}", $"{photoper:###.##}");
+
+
+            var builder = new StringBuilder();
+            builder.AppendLine($"{tweets}{perhour}{permin}{persec}|{emojis}{percent}|{hashtags}{percent}|{urls}{percent}|{photos}{percent}");
+            builder.AppendLine($"{Count,10}{hourStr,7}{minStr,7}{sec,7}|{WithEmojis,7}{emjperstr,7}|{WithHashTags,8}{hashperstr,7}|{WithUrls,7}{urlperstr,7}|{WithMedia,7}{photoperstr,7}");
+            var statsString = builder.ToString();
             return statsString;
         }
     }
---
+++ TwitterStream/TwitterMonitor.cs
@@ -0,0 +1,196 @@
+﻿using NeoSmart.Unicode;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Core.Core.Helpers;
+
+namespace TwitterStream
+{
+    public class TwitterMonitor
+    {
+        private List<Emojis> Emojis;
+        private static Dictionary<string, int> EmojisDic;
+        private static Dictionary<string, int> HashTagsDic;
+        private static Dictionary<string, int> UrlsDic;
+
+        private static HashSet<string> EmojiTexts;
+        private static HashSet<string> Unified;
+
+        private static Counters Counters;
+
+        public TwitterMonitor()
+        {
+            EmojisDic = new Dictionary<string, int>();
+            HashTagsDic = new Dictionary<string, int>();
+            UrlsDic = new Dictionary<string, int>();
+
+            EmojiTexts = new HashSet<string>();
+            Unified = new HashSet<string>();
+            Counters = new Counters();
+        }
+
+        public void Start()
+        {
+            try
+            {
+                string json = File.ReadAllText("emojis.json");
+                Emojis = JsonConvert.DeserializeObject<List<Emojis>>(json);
+
+
+                foreach (var e in Emojis)
+                {
+                    EmojiTexts.Add(e.short_name.ToLower());
+
+                    Unified.Add(e.unified);
+                }
+
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+                var apiKey = ConfigurationManager.AppSettings["APIKey"].ToString();
+                var apiKeySecrete = ConfigurationManager.AppSettings["APISecretKey"].ToString();
+                var accessToken = ConfigurationManager.AppSettings["AccessToken"].ToString();
+                var accessTokenSecrete = ConfigurationManager.AppSettings["AccessTokenSecret"].ToString();
+
+                Auth.SetUserCredentials(apiKey, apiKeySecrete, accessToken, accessTokenSecrete);
+
+                var stream = Tweetinvi.Stream.CreateSampleStream();
+                stream.TweetReceived += (sender, args) =>
+                {
+                    if (args.Tweet.Entities.Hashtags.Any())
+                    {
+                        Counters.WithHashTags++;
+
+                        var hashtags = args.Tweet.Hashtags;
+
+                        foreach (var hashtag in hashtags)
+                        {
+                            if (!HashTagsDic.ContainsKey(hashtag.Text))
+                            {
+                                HashTagsDic.Add(hashtag.Text, 1);
+                            }
+                            else
+                            {
+                                var c = HashTagsDic[hashtag.Text];
+                                c++;
+                                HashTagsDic[hashtag.Text] = c;
+                            }
+                        }
+                    }
+
+                    //  sample tweets with emojis
+                    // "@BTS_twt JOONI JJANG💜💜💜"
+                    // "Good morning 😁"
+
+                    bool hasEmojis = false;
+
+                    var emlist = Extractor.ExtractEmojis(args.Tweet.Text);
+
+                    if (emlist.Any())
+                    {
+                        foreach (var e in emlist)
+                        {
+                            hasEmojis = true;
+                            var bytes = Encoding.UTF8.GetBytes(e);
+                            var unicode = Encoding.Unicode.GetString(bytes);
+
+                            if (!EmojisDic.ContainsKey(unicode))
+                            {
+                                EmojisDic.Add(unicode, 1);
+                            }
+                            else
+                            {
+                                var c = EmojisDic[unicode];
+                                c++;
+                                EmojisDic[unicode] = c;
+                            }
+                        }
+                    }
+
+                    if (hasEmojis) Counters.WithEmojis++;
+
+                    List<string> urlsList = new List<string>();
+
+                    bool hasUrls = false;
+                    if (args.Tweet.Entities.Urls.Any())
+                    {
+                        hasUrls = true;
+
+                        foreach (var ent in args.Tweet.Entities.Urls)
+                            urlsList.Add(ent.URL);
+                    }
+
+                    if (hasUrls) Counters.WithUrls++;
+
+                    if (args.Tweet.Media.Any())
+                    {
+                        Counters.WithMedia++;
+                    }
+
+                    foreach (var u in urlsList)
+                    {
+                        if (!UrlsDic.ContainsKey(u))
+                        {
+                            UrlsDic.Add(u, 1);
+                        }
+                        else
+                        {
+                            var c = UrlsDic[u];
+                            c++;
+                            UrlsDic[u] = c;
+                        }
+                    }
+
+                    Counters.Count += 1;
+                    if (Counters.Count % 100 == 0)
+                    {
+                        DisplayResult();
+                    }
+                };
+                stream.StartStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        private static void DisplayResult()
+        {
+            Console.WriteLine(Counters.GetStatsAsString());
+            var top5str = string.Format("{0, 25}", "Top 5");
+
+            if (EmojisDic.Count > 0)
+            {
+                Console.WriteLine($"\r\n{top5str} Emojis");
+                var top5 = Extractor.GetTopFromDictionary(EmojisDic, 5);
+                foreach (var rec in top5)
+                    Console.WriteLine($"{rec.Item1,30}");
+            }
+
+            if (HashTagsDic.Count > 0)
+            {
+                Console.WriteLine($"\r\n{top5str} Hashtags");
+                var top5 = Extractor.GetTopFromDictionary(HashTagsDic, 5);
+                foreach (var rec in top5)
+                    Console.WriteLine($"{rec.Item1,35}");
+            }
+
+            if (UrlsDic.Count > 0)
+            {
+                Console.WriteLine($"\r\n{top5str} URLs");
+                var top5 = Extractor.GetTopFromDictionary(UrlsDic, 5);
+                foreach (var rec in top5)
+                    Console.WriteLine($"{rec.Item1,45}");
+            }
+
+            Console.WriteLine("\r\nPress Ctrl-C to exit.\r\n");
+        }
+    }
+}
