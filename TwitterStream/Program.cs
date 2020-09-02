ad0c4b39b9fd08daf42675869049532f80fe8678
mlacrosse3 <mlacrosse3@gmail.com>
2020-09-02 18:41:26 -0500

    More cleanup

--- TwitterStream/Program.cs
+++ TwitterStream/Program.cs
@@ -1,14 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using Tweetinvi;
+﻿using System;
 
 namespace TwitterStream
 {
